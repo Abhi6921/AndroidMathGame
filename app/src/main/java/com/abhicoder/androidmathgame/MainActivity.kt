4e9f1a6858aff0d9009f2cd02492714a7b15c692
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2022-05-06 13:11:25 +0200

    addition functionality setup

--- app/src/main/java/com/abhicoder/androidmathgame/MainActivity.kt
+++ app/src/main/java/com/abhicoder/androidmathgame/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.abhicoder.androidmathgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var additionButton : Button
+    lateinit var subtractionButton : Button
+    lateinit var multiplicationButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        additionButton = findViewById(R.id.btnAdd)
+        subtractionButton = findViewById(R.id.btnSubtract)
+        multiplicationButton = findViewById(R.id.btnMultiply)
+
+        additionButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
