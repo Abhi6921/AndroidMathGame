43bf6515b25779f66c0e069a7ee5657c5192439d
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2022-05-22 21:54:48 +0200

    even_or_odd activity

---
+++ app/src/main/java/com/abhicoder/androidmathgame/EvenOrOddActivity.kt
@@ -0,0 +1,11 @@
+package com.abhicoder.androidmathgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EvenOrOddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_even_or_odd)
+    }
+}
