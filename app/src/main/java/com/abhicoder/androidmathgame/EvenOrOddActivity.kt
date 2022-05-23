5f1fa86b359b641b616afc3e487b24958a903a32
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2022-05-23 21:25:59 +0200

    assigned ids to layouts and others

--- app/src/main/java/com/abhicoder/androidmathgame/EvenOrOddActivity.kt
+++ app/src/main/java/com/abhicoder/androidmathgame/EvenOrOddActivity.kt
@@ -2,10 +2,29 @@ package com.abhicoder.androidmathgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
 
 class EvenOrOddActivity : AppCompatActivity() {
+
+    lateinit var evenOddScore : TextView
+    lateinit var evenOddLives : TextView
+    lateinit var evenOddNumber : TextView
+    lateinit var evenRadioButton : RadioButton
+    lateinit var oddRadioButton: RadioButton
+    lateinit var buttonSubmit: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_even_or_odd)
+
+        evenOddScore = findViewById(R.id.txtevenOddScore)
+        evenOddLives = findViewById(R.id.txtevenOddLivesScore)
+        evenOddNumber = findViewById(R.id.textViewNumber)
+        evenRadioButton = findViewById(R.id.radioButtonEven)
+        oddRadioButton = findViewById(R.id.radioButtonOdd)
+        buttonSubmit = findViewById(R.id.btnSubmit)
+
     }
 }
